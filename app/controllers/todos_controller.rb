8e82f03ad5b1ac926f22a4a9f43159f60a595cf2
vagrant <vagrant@rails-dev-box>
2014-04-02 22:43:05 +0000

    updated crud functions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,7 +10,30 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
+  
+  def create
+      @todo = Todo.new(params[:todo])
+  if @todo.save
+    flash[:notice] = "Todo List was saved."
+    redirect_to @todo
+  else
+    flash[:error] = "There was an error saving the Todo List. Please try again."
+    render :new
+  end
 
+  end
   def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @post = Todo.find(params[:id])
+    if @todo.update_attributes(params[:todo])
+      flash[:notice] = "Todo was updated."
+      redirect_to @todo
+    else
+      flash[:error] = "Todo list not saved. Please try again."
+      render :edit
+    end
   end
 end
